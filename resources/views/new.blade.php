c15087175a9d962102cfc075bc2bb1e1839362b4
antallen <antallen@gmail.com>
2020-02-17 17:25:36 +0800

    add New

---
+++ resources/views/new.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+@section('title','客戶列表')
+@section('content')
+<div class="row justify-content-center">
+<div class="col-md-10">
+    <div class="card">
+    <div class="card-header">新增客戶</div>
+    <div class="card-body p-1">
+        <table class="table table-hover m-0">
+        <thead class="thead-darty">
+            <tr>
+            <th>客戶編號</th>
+            <th>客戶姓名</th>
+            <th>客戶電話</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+        </table>
+    </div>  
+    </div>
+</div>
+</div>
+@stop
