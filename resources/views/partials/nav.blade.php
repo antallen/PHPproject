59a6bcb10a2db5e9b4dae73589b7a593e6802325
antallen <antallen@gmail.com>
2020-02-17 17:04:47 +0800

    Modify Controller

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,13 +3,17 @@
         <a href="{{ url('/') }}" class="navbar-brand">
         專案站台
         </a>
-        
-            <a href="{{ action('CustomerController@new') }}" class="nav-link">
-            新增客戶
-            </a>
-            <a href="{{ action('CustomerController@index') }}" class="nav-link">
-            客戶列表
-            </a>
-       
+        <ul class="navbar-nav ml-auto mt-4 mt-lg-0">
+            <li class="nav-item active">  
+                <a href="{{ action('CustomerController@index') }}" class="nav-link">
+                客戶列表
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ action('CustomerController@new') }}" class="nav-link">
+                    新增客戶
+                </a>
+            </li>
+        </ul>
     </div>
 </nav>
