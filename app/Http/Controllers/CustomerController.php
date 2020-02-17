f50ce62cefe12c3ce41c29893d66bf7402874583
antallen <antallen@gmail.com>
2020-02-17 16:35:20 +0800

    Modify nav

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -9,9 +9,14 @@ use App\Customer;
 
 class CustomerController extends Controller
 {
-    //
+    //客戶列表
     public function index() {
         $customers = Customer::all();
         return View::make('board',['customers' => $customers]);
+    }
+
+    //新增客戶資料
+    public function new(){
+
     }
 }
