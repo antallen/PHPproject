59245210fea75df9dec9e06e5a36e3ba81186499
antallen <antallen@gmail.com>
2020-02-18 16:07:31 +0800

    Modify add

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -2,7 +2,7 @@
 @section('title','新增客戶')
 @section('content')
 <form action="{{ action('CustomerController@store') }}" method="post">
-    
+    @csrf
     <div class="row justify-content-center">
     <div class="col-md-8">
         <div class="card">
