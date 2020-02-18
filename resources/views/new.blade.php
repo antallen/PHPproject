37b01a33bb2dd5044085c1523777e09fde0035be
antallen <antallen@gmail.com>
2020-02-18 14:20:35 +0800

    Modify add

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -4,7 +4,7 @@
 <form action="" method="POST">
     @csrf
     <div class="row justify-content-center">
-    <div class="col-md-10">
+    <div class="col-md-6">
         <div class="card">
         <div class="card-header">新增客戶</div>
         <div class="card-body p-1">
