672a643fa28e01590cb19a89f8e53c7bfec01d83
antallen <antallen@gmail.com>
2020-02-18 14:14:30 +0800

    Modify add

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 //Route::resource('customer', 'CustomerController');
 Route::get('customer','CustomerController@index');
-Route::resource('new','CustomerController@new');
+Route::get('new','CustomerController@new');
