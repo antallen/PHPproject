59e6db43cc5743fef10f355bbdf88caa01a08fc1
antallen <antallen@gmail.com>
2020-02-18 14:24:23 +0800

    Modify add

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -9,24 +9,28 @@
         <div class="card-header">新增客戶</div>
         <div class="card-body p-1">
             <div class="form-group row">
+                <div class="col-sm-2"></div>
                 <label for="Cusid" class="col-sm-2 col-form-label">客戶編號</label>
                 <div class="col-sm-8">
                     <input type="text"class="form-control" id="Cusid" name="Cusid" value="">
                 </div>
             </div>
             <div class="form-group row">
+                <div class="col-sm-2"></div>
                 <label for="Name" class="col-sm-2 col-form-label">客戶姓名</label>
                 <div class="col-sm-8">
                     <input type="text" class="form-control" id="Name" name="Name" value="">
                 </div>
             </div>
             <div class="form-group row">
+                <div class="col-sm-2"></div>
                 <label for="Address" class="col-sm-2 col-form-label">通訊地址</label>
                 <div class="col-sm-8">
                     <input type="text" class="form-control" id="Address" name="Address" value="">
                 </div>
             </div>
             <div class="form-group row">
+                <div class="col-sm-2"></div>
                 <label for="Phone" class="col-sm-2 col-form-label">連絡電話</label>
                 <div class="col-sm-8">
                     <input type="text" class="form-control" id="Phone" name="Phone" value="">
