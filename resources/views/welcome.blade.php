decef870984fe1a1c444af953438f282449689fe
antallen <antallen@gmail.com>
2020-02-19 15:53:52 +0800

    Modify Exercise

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,10 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome ! PHPproject !
                 </div>
 
                 <div class="links">
+                    <a href="https://localhost/customer">Customer</a>
+                    <!--
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -93,6 +95,8 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    -->
+                    <a href="https://github.com/antallen/PHPproject">GitHub</a>
                 </div>
             </div>
         </div>
