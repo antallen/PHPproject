b9b92a1fc9b63822e934b0204b19414740ae5884
antallen <antallen@gmail.com>
2020-02-19 11:09:53 +0800

    Modify edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -38,11 +38,11 @@
             </div>
             <div class="form-group row">
                 <div class="col-sm-1"></div>
-                <div class="col-sm-8">
+                <div class="col-sm-9">
                     <input type="submit" class="btn btn-primary" value="送出">
                     <input type="hidden" id="oldId" name="oldId"  value="<?php echo $_GET['Cusid']; ?>">
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-1">
                     <input type="submit" class="btn btn-warning" value="取消" name="cancel">
                 </div>
             </div>  
