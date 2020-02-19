2086b5567e97d55e15f98a8b545fa9d6001425b5
antallen <antallen@gmail.com>
2020-02-19 11:07:53 +0800

    Modify edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -38,7 +38,7 @@
             </div>
             <div class="form-group row">
                 <div class="col-sm-1"></div>
-                <div class="col-sm-10">
+                <div class="col-sm-8">
                     <input type="submit" class="btn btn-primary" value="送出">
                     <input type="hidden" id="oldId" name="oldId"  value="<?php echo $_GET['Cusid']; ?>">
                 </div>
