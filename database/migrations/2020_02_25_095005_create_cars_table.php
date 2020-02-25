096baa1581f6256e7880c326cf229a00b326f06c
antallen <antallen@gmail.com>
2020-02-25 18:04:52 +0800

    Modify exercise3

---
+++ database/migrations/2020_02_25_095005_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            //$table->bigIncrements('id');
+            $table->char('Carno',10)->primary();
+            $table->char('Cusid',20)->index();
+            $table->char('CarStyleid',50)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
