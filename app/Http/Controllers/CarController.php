d3680888f242f13d3870f967d7f68f96e432e2b9
antallen <antallen@gmail.com>
2020-02-26 16:59:20 +0800

    Modify Cars

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -12,11 +12,10 @@ class CarController extends Controller
 {
     //顯示車主所擁有的車輛
     public function index(Request $request) {
-        if ($request->Cusid){
+        if ($request->has('Cusid')){
             $customer = Customer::where('Cusid',$request->Cusid);
-            //$cars = Car::where('Cusid',$request->Cusid);
-            //return View::make('car',['customer' => $customer,'cars' => $cars]);
-            dd($customer);
+            $cars = Car::where('Cusid',$request->Cusid);
+            return View::make('car',['customer' => $customer,'cars' => $cars]);
         } else {
             $customers = Customer::all();
             return View::make('board',['customers' => $customers]); 
