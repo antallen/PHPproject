7a42b34be7d7b4d16d546069a05805660ba9e997
antallen <antallen@gmail.com>
2020-02-28 12:43:48 +0800

    Modify ex4

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-sm-2"></div>
                 <label for="Cusid" class="col-sm-2 col-form-label">客戶編號</label>
                 <div class="col-sm-8">
-                    <input type="text" class="form-control {{ $errors->has('Cusid') ? 'is-invalid' : '' }}" id="Cusid" name="Cusid" value="{{ $customers->Cusid }}">
+                    <input type="text" class="form-control {{ $errors->has('Cusid') ? 'is-invalid' : '' }}" id="Cusid" name="Cusid" value="{{ $Cusid }}">
                     @if ($errors->has('Cusid'))
                         <div class="invalid-feedback">
                             <strong>{{ $errors->first('Cusid')}}</strong>
@@ -32,7 +32,7 @@
                 <div class="col-sm-2"></div>
                 <label for="Name" class="col-sm-2 col-form-label">客戶姓名</label>
                 <div class="col-sm-8">
-                    <input type="text" class="form-control {{ $errors->has('Name') ? 'is-invalid' : '' }}" id="Name" name="Name" value="{{ $customers->Name }}">
+                    <input type="text" class="form-control {{ $errors->has('Name') ? 'is-invalid' : '' }}" id="Name" name="Name" value="{{ $Name }}">
                     @if ($errors->has('Name'))
                         <div class="invalid-feedback">
                             <strong>{{ $errors->first('Name')}}</strong>
@@ -44,7 +44,7 @@
                 <div class="col-sm-2"></div>
                 <label for="Address" class="col-sm-2 col-form-label">通訊地址</label>
                 <div class="col-sm-8">
-                    <input type="text" class="form-control {{ $errors->has('Address') ? 'is-invalid' : '' }}" id="Address" name="Address" value="{{ $customers->Address }}">
+                    <input type="text" class="form-control {{ $errors->has('Address') ? 'is-invalid' : '' }}" id="Address" name="Address" value="{{ $Address }}">
                     @if ($errors->has('Address'))
                         <div class="invalid-feedback">
                             <strong>{{ $errors->first('Address')}}</strong>
@@ -56,7 +56,7 @@
                 <div class="col-sm-2"></div>
                 <label for="Phone" class="col-sm-2 col-form-label">連絡電話</label>
                 <div class="col-sm-8">
-                    <input type="text" class="form-control {{ $errors->has('Phone') ? 'is-invalid' : '' }}" id="Phone" name="Phone" value="{{ $customers->Phone }}">
+                    <input type="text" class="form-control {{ $errors->has('Phone') ? 'is-invalid' : '' }}" id="Phone" name="Phone" value="{{ $Phone }}">
                     @if ($errors->has('Phone'))
                         <div class="invalid-feedback">
                             <strong>{{ $errors->first('Phone')}}</strong>
@@ -68,7 +68,7 @@
                 <div class="col-sm-1"></div>
                 <div class="col-sm-9">
                     <input type="submit" class="btn btn-primary" value="送出">
-                    <input type="hidden" id="oldId" name="oldId"  value="{{ $customers->Cusid }}">
+                    <input type="hidden" id="oldId" name="oldId"  value="{{ $Cusid }}">
                 </div>
                 <div class="col-sm-1">
                     <input type="submit" class="btn btn-warning" value="取消" name="cancel">
