be120fc616e425c6570371cbf46291f0e6e11831
antallen <antallen@gmail.com>
2020-03-14 17:16:46 +0800

    Modify CarController

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -45,8 +45,9 @@ class CarController extends Controller
         $list=$this->CarCustomerService->getCars($request->Cusid);
         $customer=$list['customer'];
         $cars=$list['cars'];
-        return redirect()->action('CarController@index',['customer'=>$customer,'cars'=>$cars]);
+        //return redirect()->action('CarController@index',['customer'=>$customer,'cars'=>$cars]);
         //return View::make('car',['customer'=>$customer,'cars'=>$cars]);
+        return redirect()->back()->withInput(['customer'=>$customer,'cars'=>$cars]);
     }
     //編輯客戶車輛程式
     public function edit(Request $request){
